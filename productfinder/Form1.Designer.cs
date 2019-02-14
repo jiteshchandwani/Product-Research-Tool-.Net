cd2b9090979d30cbe2498a9b189f672fc8e2a31b
jiteshchandwani <jiteshchandwani10@gmail.com>
2019-02-14 21:18:43 +0000

    Final

---
+++ productfinder/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace productfinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Product_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Orders = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FeedBack = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(64, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Search Products";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(44, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(178, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Product_Name,
+            this.Link,
+            this.Orders,
+            this.FeedBack,
+            this.Price});
+            this.dataGridView1.Location = new System.Drawing.Point(278, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(505, 350);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Product_Name
+            // 
+            this.Product_Name.HeaderText = "Product_Name";
+            this.Product_Name.Name = "Product_Name";
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            // 
+            // Orders
+            // 
+            this.Orders.HeaderText = "Orders";
+            this.Orders.Name = "Orders";
+            // 
+            // FeedBack
+            // 
+            this.FeedBack.HeaderText = "FeedBack";
+            this.FeedBack.Name = "FeedBack";
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Price";
+            this.Price.Name = "Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(64, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Related searches";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(529, 409);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(254, 35);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Export To CSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(174, 409);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(292, 35);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Completed !! Search Another product";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(59, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Enter Product";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 10;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(174, 379);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(609, 23);
+            this.progressBar1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 25);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Progress";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(44, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 23);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(96, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Enter proxy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(824, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Product Research Tool [Aliexpress]";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Product_Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Link;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Orders;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FeedBack;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ productfinder/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace productfinder
+{
+    using Excel = Microsoft.Office.Interop.Excel;
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           // var webGet = new HtmlWeb();
+
+            //WebClient client = new WebClient();
+            //client.Proxy = new WebProxy("201.236.143.242:56104", false);
+            //if (textBox2.Text != "")
+            //{
+            //    WebClient client = new WebClient();
+            //    // client.Proxy = new WebProxy('"' + textBox2.Text + '"', false);
+
+            //    // client.Method = "GET";
+            //}
+            string output = textBox1.Text.Replace(" ", "+");
+            WebClient wc = new WebClient();
+            wc.Proxy = new WebProxy("201.236.143.242:56104");
+            var page = wc.DownloadString("https://www.aliexpress.com/wholesale?SortType=create_desc&SearchText=" + '"' + output + '"' + "&page=" + 1 + "&isFavorite=y");
+
+            var webGet = new HtmlWeb();
+
+
+
+            var doc1 = webGet.Load(page); //webGet.Load("https://www.aliexpress.com/wholesale?SortType=create_desc&SearchText=" + '"' + output + '"' + "&page=" + 1 + "&isFavorite=y");
+            var tag1 = doc1.DocumentNode.SelectNodes("//*[contains(@class,'ui-pagination-navi util-left')]");
+            int k = 0;
+            try
+            {
+                foreach (var node in tag1)
+                {
+                    string str = node.InnerText.Trim();
+                    str = Regex.Replace(str, @"\s", "");
+                    char ch = str[str.Length - 5];
+                    str = Convert.ToString(ch);
+                    k = Convert.ToInt32(str);
+
+
+                }
+            }
+            catch (NullReferenceException ex)
+            {
+                MessageBox.Show("Change IP");
+            }
+            progressBar1.Maximum = k-1;
+            progressBar1.Step = 1;
+            progressBar1.Value = 0;
+            for (int j = 0; j < k; j++) { 
+            var doc = webGet.Load("https://www.aliexpress.com/wholesale?SortType=create_desc&SearchText=" + '"' + output + '"'+ "&page="+ j +"&isFavorite=y");
+
+                if (j == 0)
+                {
+                    var tag = doc.DocumentNode.SelectNodes("//*[contains(@class,'pop-keyword-diva')]");
+                    try { 
+                    foreach (var node in tag)
+                    {
+
+                            label1.Text = node.InnerText;
+                    }
+                }
+            catch (NullReferenceException ex)
+                {
+                    MessageBox.Show("Change IP");
+                }
+
+            }
+            int i=0;
+                
+                var item = doc.DocumentNode.SelectNodes("//div[contains(@class,'info')]");
+                try
+                {
+                    foreach (var node in item)
+                    {
+                        try
+                        {
+                            dataGridView1.Rows.Add(new object[] { node.SelectSingleNode(".//*[contains(@class,'history-item product')]").InnerText, node.SelectSingleNode(".//*[contains(@class,'history-item product')]").Attributes["href"].Value.Remove(0,2), node.SelectSingleNode(".//*[contains(@class,'order-num-a')]").InnerText.Replace("Orders (","").Replace(")","").Replace("Order (", ""), node.SelectSingleNode(".//*[contains(@class,'rate-num')]").InnerText.Replace("(", "").Replace(")",""), Regex.Replace(node.SelectSingleNode(".//*[contains(@class,'price price-m')]").InnerText.Replace("piece","").Replace("/",""), @"\s", "") });
+                        }
+                        catch (NullReferenceException ex)
+                        {
+                           
+                        }
+                        i = i + 1;
+                    }
+                }
+                catch (NullReferenceException ex)
+                {
+                    MessageBox.Show("Not all Products are selected. Change IP");
+                }
+                progressBar1.Value = j;
+                
+                
+            }
+            button3.Visible = true;
+            button2.Visible = true;
+    }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void copyAlltoClipboard()
+        {
+            dataGridView1.SelectAll();
+            DataObject dataObj = dataGridView1.GetClipboardContent();
+            if (dataObj != null)
+                Clipboard.SetDataObject(dataObj);
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                copyAlltoClipboard();
+                Microsoft.Office.Interop.Excel.Application xlexcel;
+                Microsoft.Office.Interop.Excel.Workbook xlWorkBook;
+                Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+                xlexcel = new Excel.Application();
+                xlexcel.Visible = true;
+                xlWorkBook = xlexcel.Workbooks.Add(misValue);
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+                Excel.Range CR = (Excel.Range)xlWorkSheet.Cells[1, 1];
+                CR.Select();
+                xlWorkSheet.PasteSpecial(CR, Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing, true);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Install MS Excel first and then try");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button3.Visible = false;
+            button2.Visible = false;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.ColumnHeadersVisible = false;
+            dataGridView1.Rows.Add("Product Name", "Product Link", "Number of Orders", "Number of Feedback", "Price");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form1 NewForm = new Form1();
+            NewForm.Show();
+            this.Dispose(false);
+        }
+    }
+}
